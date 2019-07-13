4b43fcb93f5d8cf6925aa81d8250f9b1eb6eab74
Andrew Carpenter <andrew@audioinhd.com>
2019-07-13 15:41:25 -0400

    Implement all assertions

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
 'use strict'
 
+const { deepStrictEqual } = require('assert').strict
 const checkChar = require('./cli-char-supported')
 const colorReset = `\u001b[0m`
 const colorGreen = `\u001b[32m`
 const colorRed = `\u001b[31m`
-let verbose = process.argv.some((a) => a === '--verbose')
+const colorYellow = `\u001b[33m`
+const queue = []
 
-let hasEmojiGood
-let hasEmojiFail
+let verbose = process.argv.some((a) => a === '--verbose' || a === '-v')
 
-const emojiGood = () => hasEmojiGood ? '\u2705' : `${colorGreen}[√]${colorReset}`
-const emojiFail = () => hasEmojiFail ? '\u274C' : `${colorRed}[X]${colorReset}`
-
-const test = async (msg, fn) => {
-  await fn(assert(msg))
-  if (verbose) process.stdout.write(`${colorReset}${emojiGood()} ${msg}\n`)
+let charsChecked = false
+const chars = {
+  good: { emoji: '✅', plain: `${colorGreen}[√]` },
+  fail: { emoji: '❌', plain: `${colorRed}[X]` },
+  skip: { emoji: '⛔', plain: `${colorYellow}[-]` },
+  okFail: { emoji: '❎', plain: `${colorGreen}[X]` }
 }
 
-const setup = async (opts = {}) => {
+const char = (n) => {
+  return `${chars[n].useEmoji ? chars[n].emoji : chars[n].plain}${colorReset}`
+}
+
+const setup = (opts = {}) => before(async () => {
   verbose = opts.verbose !== undefined ? !!opts.verbose : verbose
 
-  if (hasEmojiGood !== undefined && hasEmojiFail !== undefined) return
-  hasEmojiGood = (await checkChar('\u2705', 2)) || false
-  hasEmojiFail = (await checkChar('\u274C', 2)) || false
+  if (charsChecked) return
+
+  for (let c of Object.values(chars)) {
+    c.useEmoji = await checkChar(c.emoji, 2)
+  }
+
+  charsChecked = true
+})
+
+const runner = async ({ msg, fn }) => {
+  await fn(assert(msg))
+  if (!msg || !verbose) return
+  process.stdout.write(`${char('good')} ${msg}\n`)
 }
 
-const wrap = (msg, passed, err) => {
-  if (passed) return
+const test = async (msg, fn) => {
+  if (msg && fn) queue.push({ msg, fn })
 
-  process.stderr.write(`${colorReset}${emojiFail()} ${msg}\n`)
-  console.error(err instanceof Error ? err : new Error(err))
+  const curLen = queue.length
+  process.nextTick(async () => {
+    if (curLen !== queue.length) return
 
-  return process.exit(1)
+    for (let t of queue) await runner(t)
+  })
 }
 
-const before = () => {}
+const before = (fn) => {
+  queue.unshift({ fn })
+  test()
+  return test
+}
 
 const after = () => {}
 
 const serial = () => {}
 
-const skip = () => {}
+const skip = (msg) => {
+  const fn = () => {
+    const toPrint = `${char('skip')} ${colorYellow}${msg}${colorReset}\n`
+    process.stdout.write(toPrint)
+  }
+  queue.push({ fn })
+  test()
+  return test
+}
 
 const only = () => {}
 
@@ -56,19 +85,47 @@ const assert = (msg) => ({
   deepEqual: deepEqual(msg)
 })
 
-const is = (msg) => (a, b) => wrap(msg, Object.is(a, b), `${a} !== ${b}`)
+const toPrint = (s) => typeof s === 'string' ? `'${s}'` : s
 
-const not = (msg) => () => {}
+const wrap = (msg, passFn, err) => {
+  let passed = false
+  try {
+    passed = passFn()
+  } catch (ex) {
+    err = ex
+  }
 
-const pass = (msg) => () => {}
+  if (passed) return assert(msg)
 
-const fail = (msg) => () => {}
+  process.stderr.write(`${char('fail')} ${msg}\n`)
+  console.error(err instanceof Error ? err : new Error(err))
 
-const truthy = (msg) => () => {}
+  return process.exit(1)
+}
 
-const falsy = (msg) => () => {}
+const is = (msg) => (a, b) => {
+  return wrap(msg, () => Object.is(a, b), `${toPrint(a)} !== ${toPrint(b)}`)
+}
 
-const deepEqual = (msg) => () => {}
+const not = (msg) => (a, b) => {
+  return wrap(msg, () => !Object.is(a, b), `${toPrint(a)} === ${toPrint(b)}`)
+}
+
+const pass = (msg) => () => wrap(msg, () => true)
+
+const fail = (msg) => () => wrap(msg, () => false)
+
+const truthy = (msg) => (a) => wrap(msg, () => !!a, `not truthy: ${toPrint(a)}`)
+
+const falsy = (msg) => (a) => wrap(msg, () => !a, `not falsy: ${toPrint(a)}`)
+
+const deepEqual = (msg) => (a, b) => {
+  return wrap(
+    msg,
+    () => deepStrictEqual(a, b) || true,
+    `not deepEqual:\nA:\n${toPrint(a)}\nB:\n${toPrint(b)}`
+  )
+}
 
 Object.assign(test, {
   assert,
@@ -80,5 +137,7 @@ Object.assign(test, {
   only,
   failing
 })
+
+setup()
 
 module.exports = test
--- test.js
+++ test.js
@@ -3,6 +3,45 @@
 const test = require('./index')
 
 test('assert.is works', async (assert) => {
-  await test.setup({ verbose: true })
   assert.is(1, 1)
+  assert.is(true, true)
+  assert.is('a', 'a')
+})
+
+test('assert.not works', async (assert) => {
+  assert.not(1, 2)
+  assert.not(true, false)
+  assert.not('a', 'b')
+})
+
+test('assert.pass works', async (assert) => {
+  assert.pass()
+})
+
+test.skip('assert.fail works', async (assert) => {
+  assert.fail()
+})
+
+test('assert.truthy works', async (assert) => {
+  assert.truthy(1)
+  assert.truthy(true)
+  assert.truthy('a')
+})
+
+test('assert.falsy works', async (assert) => {
+  assert.falsy(0)
+  assert.falsy('')
+  assert.falsy(false)
+  assert.falsy(null)
+  assert.falsy(undefined)
+  assert.falsy()
+})
+
+test('assert.deepEqual works', async (assert) => {
+  assert.deepEqual(1, 1)
+  assert.deepEqual([1, 2, 3], [1, 2, 3])
+  assert.deepEqual(
+    { a: [1, 2], b: { a: { c: 2 } } },
+    { a: [1, 2], b: { a: { c: 2 } } }
+  )
 })
